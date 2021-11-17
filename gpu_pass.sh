726d9d5cd95ba162658437016434c7f988d11c19
Awnrt <roman.levikoff@gmail.com>
2021-11-17 15:43:59 +0000

    Updated

--- gpu_pass.sh
+++ gpu_pass.sh
@@ -1,1 +1,7 @@
 sudo pacman -S qemu libvirt edk2-ovmf virt-manager ebtables dnsmasq
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
+sudo systemctl enable virtlogd.socket
+sudo systemctl start virtlogd.socket
+sudo virsh net-autostart default
+sudo virsh net-start default
